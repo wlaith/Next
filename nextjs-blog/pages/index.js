510513c6ea73a4121899a77cee0c55ac618012c6
wlaith <laethwazani@gmail.com>
2023-03-03 00:04:51 +0300

    test: does pgp key work

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Learning <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
